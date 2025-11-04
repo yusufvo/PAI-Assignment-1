163fe1611b460d8d49aad6a991496ee812d569de
yusufvo <k230079@nu.edu.pk>
2025-11-04 10:24:06 -0800

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,22 +1,25 @@
 import time
+
 class package:
     def __init__(self, packageid, weightinkg):
         self.packageid = packageid
         self.weightinkg = weightinkg
+
 class drone:
-    statuses={'idle','delivering','charging'}
+    statuses = {'idle', 'delivering', 'charging'}
+    
     def __init__(self, droneid, maxload):
         self.droneid = droneid
         self.maxload = maxload
         self.__status = 'idle'
         self.__timer = 0
-        self.__currentpackage =  None
+        self.__currentpackage = None
 
     def getstatus(self):
         return self.__status
 
     def setstatus(self, newstatus):
-        if newstatus in self.valid_statuses:
+        if newstatus in drone.statuses:
             self.__status = newstatus
             
             if newstatus == 'delivering':
@@ -36,8 +39,8 @@ class drone:
         if self.getstatus() != 'idle':
             return False
             
-        if packageobj.weightinkg > self.maxloadinkg:
-            print(f"drone {self.droneid}: package {packageobj.packageid} ({packageobj.weightinkg}kg) is too heavy (max {self.maxloadinkg}kg).")
+        if packageobj.weightinkg > self.maxload:
+            print(f"drone {self.droneid}: package {packageobj.packageid} ({packageobj.weightinkg}kg) is too heavy (max {self.maxload}kg).")
             return False
             
         self.__currentpackage = packageobj
@@ -72,7 +75,7 @@ class fleetmanager:
 
     def dispatchjobs(self):
         print("\n*dispatching jobs")
-        toberemovedpackages=[]
+        toberemovedpackages = []
         for package in self.pendingpackages:
             for droneid, drone in self.drones.items():
                 if drone.assignpackage(package):
@@ -80,10 +83,12 @@ class fleetmanager:
                     break
         for package in toberemovedpackages:
             self.pendingpackages.remove(package)
+
     def simulationtick(self):
         print(f"\n*simulation tick")
         for droneid, drone in self.drones.items():
             drone.updatetick()
+
 if __name__ == "__main__":
 
     print("*initializing fleet")
